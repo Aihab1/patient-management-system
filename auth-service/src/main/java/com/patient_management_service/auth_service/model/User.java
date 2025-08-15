634797537016071c5e40b90efd3255587d3548a4
Aihab Umair <addisonhardley111@gmail.com>
2025-08-15 13:40:42 +0530

    Setup auth service

---
+++ auth-service/src/main/java/com/patient_management_service/auth_service/model/User.java
@@ -0,0 +1,26 @@
+package com.patient_management_service.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
